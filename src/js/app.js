cb30600225698ef93108143d97c12a53e8ffeb78
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2020-02-06 15:42:47 +0100

    Add css improvements

--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,39 @@
 App = {
   web3Provider: null,
   contracts: {},
-  pingFile: 'MyPing.json',
-  //pingFile:'@element36-io/cash36-contracts/build/contracts/Ping.json',
-  getAmount: () => { return  $("#amount")[0].value },
-  getBeneficiary: function () {  return $("#beneficiary")[0].value },
-  format: (value) => { return value * 1000000000000000000 },
-  parse: (value) => { return value / 1000000000000000000 },
+  // pingFile: 'MyPing.json',
+  pingFile: '@element36-io/cash36-contracts/build/contracts/Ping.json',
+  getAmount: () => {
+    return $('#amount')[0].value;
+  },
+  getBeneficiary: function() {
+    return $('#beneficiary')[0].value;
+  },
+  format: value => {
+    return value * 1000000000000000000;
+  },
+  parse: value => {
+    return value / 1000000000000000000;
+  },
 
-  init: async function () {
-    console.log("initWeb3 0");
+  init: async function() {
+    console.log('initWeb3 0');
     await App.initWeb3();
-    console.log("updateUserData");
+    console.log('updateUserData');
     return await App.updateUserData();
   },
 
-  initWeb3: async function () {
+  initWeb3: async function() {
     // Modern dapp browsers...
     if (window.ethereum) {
-      console.log(".. Metamask or similar provides web3 ")
+      console.log('.. Metamask or similar provides web3 ');
       App.web3Provider = window.ethereum;
       try {
         // Request account access - opens MetaMask
         await window.ethereum.enable();
       } catch (error) {
         // User denied account access...
-        console.error("User denied account access")
+        console.error('User denied account access');
       }
     }
     // Legacy dapp browsers...
@@ -34,168 +42,226 @@ App = {
     }
     // If no injected web3 instance is detected, fall back to Ganache
     else {
-      console.warn(" ==> fallback, no web3 object. Taking provider from " + $("#providerUrl")[0].value)
-      App.web3Provider = new Web3.providers.HttpProvider($("#providerUrl")[0].value);
+      console.warn(
+        ' ==> fallback, no web3 object. Taking provider from ' +
+          $('#providerUrl')[0].value
+      );
+      App.web3Provider = new Web3.providers.HttpProvider(
+        $('#providerUrl')[0].value
+      );
     }
     return App.initContract();
   },
 
-  initContract: function () {
-    console.log("initContract")
+  initContract: function() {
+    console.log('initContract');
     //pingFile:'MyPing.json',
     //pingFile:'@element36-io/cash36-contracts/build/contracts/Ping.json',
-    $.getJSON(App.pingFile, function (data) {
+    $.getJSON(App.pingFile, function(data) {
       App.contracts.Ping = TruffleContract(data);
       App.contracts.Ping.setProvider(App.web3Provider);
       App.contracts.Ping.deployed().then(() =>
-        console.log(" > App.contracts.Ping.address:" + App.contracts.Ping.address))
+        console.log(
+          ' > App.contracts.Ping.address:' + App.contracts.Ping.address
+        )
+      );
     });
-    $.getJSON("@element36-io/cash36-contracts/build/contracts/CHF36.json", function (data) {
-      App.contracts.chf36 = TruffleContract(data);
-      App.contracts.chf36.setProvider(App.web3Provider);
-      App.contracts.chf36.deployed().then(() =>
-        console.log(" > App.contracts.chf36.address:" + App.contracts.chf36.address))
-    });
-    $.getJSON("@element36-io/cash36-contracts/build/contracts/EUR36.json", function (data) {
-      App.contracts.eur36 = TruffleContract(data);
-      // Set the provider for our contract
-      App.contracts.eur36.setProvider(App.web3Provider);
-      App.contracts.eur36.deployed().then(() =>
-        console.log(" > App.contracts.eur36.address:" + App.contracts.eur36.address))
-    });
-    $.getJSON("@element36-io/cash36-contracts/build/contracts/Cash36Compliance.json", function (data) {
-      App.contracts.compliance = TruffleContract(data);
-      // Set the provider for our contract
-      App.contracts.compliance.setProvider(App.web3Provider);
-      App.contracts.compliance.deployed().then(() =>
-        console.log(" > App.contracts.compliance.address:" + App.contracts.compliance.address))
-    });
+    $.getJSON(
+      '@element36-io/cash36-contracts/build/contracts/CHF36.json',
+      function(data) {
+        App.contracts.chf36 = TruffleContract(data);
+        App.contracts.chf36.setProvider(App.web3Provider);
+        App.contracts.chf36
+          .deployed()
+          .then(() =>
+            console.log(
+              ' > App.contracts.chf36.address:' + App.contracts.chf36.address
+            )
+          );
+      }
+    );
+    $.getJSON(
+      '@element36-io/cash36-contracts/build/contracts/EUR36.json',
+      function(data) {
+        App.contracts.eur36 = TruffleContract(data);
+        // Set the provider for our contract
+        App.contracts.eur36.setProvider(App.web3Provider);
+        App.contracts.eur36
+          .deployed()
+          .then(() =>
+            console.log(
+              ' > App.contracts.eur36.address:' + App.contracts.eur36.address
+            )
+          );
+      }
+    );
+    $.getJSON(
+      '@element36-io/cash36-contracts/build/contracts/Cash36Compliance.json',
+      function(data) {
+        App.contracts.compliance = TruffleContract(data);
+        // Set the provider for our contract
+        App.contracts.compliance.setProvider(App.web3Provider);
+        App.contracts.compliance
+          .deployed()
+          .then(() =>
+            console.log(
+              ' > App.contracts.compliance.address:' +
+                App.contracts.compliance.address
+            )
+          );
+      }
+    );
 
-    console.log("initContract done")
+    console.log('initContract done');
     return App.bindEvents();
   },
 
-  bindEvents: function () {
+  bindEvents: function() {
     $(document).on('click', '.btn-ping', App.handlePing2);
     $(document).on('click', '.btn-pong', App.handlePong);
     $(document).on('click', '.btn-refresh', App.updateUserData);
     $(document).on('click', '.btn-withdraw', App.handleWithdraw);
   },
 
- 
-  handlePing2: function () {
-    App.handlePing()
+  handlePing2: function() {
+    App.handlePing();
   },
 
-  handlePing: async function (event) {
+  handlePing: async function(event) {
     if (event) event.preventDefault();
 
     const pingInstance = await App.contracts.Ping.deployed();
     const chf36Instance = await App.contracts.chf36.deployed();
 
-    let ping = await chf36Instance.approve(pingInstance.address, App.format(App.getAmount()), { from: App.account })
-      .then(() => {
-        return pingInstance.ping(App.getBeneficiary(), App.format(App.getAmount()), { from: App.account });
+    let ping = await chf36Instance
+      .approve(pingInstance.address, App.format(App.getAmount()), {
+        from: App.account
       })
-    App.updateUserData()
+      .then(() => {
+        return pingInstance.ping(
+          App.getBeneficiary(),
+          App.format(App.getAmount()),
+          { from: App.account }
+        );
+      });
+    App.updateUserData();
   },
 
-  handlePong: function (event) {
-    console.log(".. click pong");
+  handlePong: function(event) {
+    console.log('.. click pong');
     event.preventDefault();
     let pingInstance;
 
-    web3.eth.getAccounts(function (error, accounts) {
+    web3.eth.getAccounts(function(error, accounts) {
       if (error) {
         console.log(error);
       }
 
-      App.contracts.Ping.deployed().then(function (instance) {
-        pingInstance = instance;
-        // Execute adopt as a transaction by sending account
-        pingInstance.pong().then(function (r) {
-          console.log("done: " + r), (error) => {
-            console.log(error)
-          }
+      App.contracts.Ping.deployed()
+        .then(function(instance) {
+          pingInstance = instance;
+          // Execute adopt as a transaction by sending account
+          pingInstance
+            .pong()
+            .then(function(r) {
+              console.log('done: ' + r),
+                error => {
+                  console.log(error);
+                };
+            })
+            .catch(function(err) {
+              console.log(err.message);
+            });
         })
-          .catch(function (err) {
-            console.log(err.message);
-          });
-      }).then(function (result) {
-        return App.updateUserData();
-      }).catch(function (err) {
-        alert("Error " + err.message)
-        console.log(err.message);
-      });
+        .then(function(result) {
+          return App.updateUserData();
+        })
+        .catch(function(err) {
+          alert('Error ' + err.message);
+          console.log(err.message);
+        });
     });
   },
-  handleWithdraw: async function (event) {
+  handleWithdraw: async function(event) {
     event.preventDefault();
     const pingInstance = await App.contracts.Ping.deployed();
-    await pingInstance.steal({ from: App.account })
-    App.updateUserData()
+    await pingInstance.steal({ from: App.account });
+    App.updateUserData();
   },
-  updateUserData: async function () {
+  updateUserData: async function() {
+    $('#account')[0].innerHTML = '...';
+    $('#balanceEth')[0].innerHTML = '...';
+    $('#balance36')[0].innerHTML = '...';
+    $('#beneficiaryBalance36')[0].innerHTML = '...';
+    $('#contractBalance36')[0].innerHTML = '...';
 
-    $("#account")[0].innerHTML = "..."
-    $("#balanceEth")[0].innerHTML = "..."
-    $("#balance36")[0].innerHTML = "..."
-    $("#beneficiaryBalance36")[0].innerHTML = "..."
-    $("#contractBalance36")[0].innerHTML = "..."
-
-    // MILOS... how can we call this after init? Could not figure out how 
+    // MILOS... how can we call this after init? Could not figure out how
     // to do this without a break in case of a pageload - my javascript is too rusty
     await new Promise(r => setTimeout(r, 2000));
 
     //get eth
     web3 = new Web3(App.web3Provider);
     App.account = web3.eth.accounts[0];
-    $("#account")[0].innerHTML = App.account;
+    $('#account')[0].innerHTML = App.account;
 
     web3.eth.getBalance(App.account, (err, balance) => {
-      $("#balanceEth")[0].innerHTML = web3.fromWei(balance, "ether") + " ETH"
-      if (err) $("#balanceEth")[0].innerHTML = err
+      $('#balanceEth')[0].innerHTML = web3.fromWei(balance, 'ether') + ' ETH';
+      if (err) $('#balanceEth')[0].innerHTML = err;
     });
 
-    $('#balance36')[0].innerHTML = JSON.stringify(await App.getUserStatus(App.account))
+    $('#balance36')[0].innerHTML = JSON.stringify(
+      await App.getUserStatus(App.account)
+    );
 
-    $('#beneficiaryBalance36')[0].innerHTML = JSON.stringify(await App.getUserStatus(App.getBeneficiary()))
-    
+    $('#beneficiaryBalance36')[0].innerHTML = JSON.stringify(
+      await App.getUserStatus(App.getBeneficiary())
+    );
+
     const pingInstance = await App.contracts.Ping.deployed();
     const cBalanceChf36 = await chf36.balanceOf(pingInstance.address);
     const cBeneficiary = await pingInstance.beneficiary();
-    $('#contractBalance36')[0].innerHTML = App.parse(cBalanceChf36) + " CHF36, beneficiary: " + cBeneficiary + ", contract: " + App.contracts.chf36.address;
-
+    $('#contractBalance36')[0].innerHTML =
+      App.parse(cBalanceChf36) +
+      ' CHF36, beneficiary: ' +
+      cBeneficiary +
+      ', contract: ' +
+      App.contracts.chf36.address;
   },
 
-  getUserStatus: async function (address) {
+  getUserStatus: async function(address) {
     let result = {};
-    result["address"] = address;
+    result['address'] = address;
     chf36 = await App.contracts.chf36.deployed();
-    result["balanceChf36"] = App.parse(await chf36.balanceOf(address));
+    result['balanceChf36'] = App.parse(await chf36.balanceOf(address));
     eur36 = await App.contracts.eur36.deployed();
-    result["balanceEur36"] = App.parse(await eur36.balanceOf(address));
+    result['balanceEur36'] = App.parse(await eur36.balanceOf(address));
     compliance = await App.contracts.compliance.deployed();
 
-    result["ATTR_BUY"] = await compliance.hasAttribute(address, "ATTR_BUY");
+    result['ATTR_BUY'] = await compliance.hasAttribute(address, 'ATTR_BUY');
 
-    result["ATTR_SEND"] = await compliance.hasAttribute(address, "ATTR_SEND");
-    result["ATTR_RECEIVE"] = await compliance.hasAttribute(address, "ATTR_RECEIVE");
-    result["ATTR_SELL"] = await compliance.hasAttribute(address, "ATTR_SELL");
+    result['ATTR_SEND'] = await compliance.hasAttribute(address, 'ATTR_SEND');
+    result['ATTR_RECEIVE'] = await compliance.hasAttribute(
+      address,
+      'ATTR_RECEIVE'
+    );
+    result['ATTR_SELL'] = await compliance.hasAttribute(address, 'ATTR_SELL');
 
+    result['isOnBlacklist'] = await compliance.isOnBlacklist(address);
+    result['isOnLockedAccounts'] = await compliance.isOnLockedAccounts(address);
 
-    result["isOnBlacklist"] = await compliance.isOnBlacklist(address);
-    result["isOnLockedAccounts"] = await compliance.isOnLockedAccounts(address);
-
-    result["limgetUserLimitit"] = await compliance.getUserLimit(address);
-    result["checkUserLimit"] = await compliance.checkUserLimit(address, App.format(App.getAmount()), result["balanceEur36"]);
-    result["checkUser"] = await compliance.checkUser(address);
-    console.log(" " + address + ": " + JSON.stringify(result));
+    result['limgetUserLimitit'] = await compliance.getUserLimit(address);
+    result['checkUserLimit'] = await compliance.checkUserLimit(
+      address,
+      App.format(App.getAmount()),
+      result['balanceEur36']
+    );
+    result['checkUser'] = await compliance.checkUser(address);
+    console.log(' ' + address + ': ' + JSON.stringify(result));
     return result;
   }
 }; //App
-$(function () {
-  $(window).load(function () {
+$(function() {
+  $(window).load(function() {
     App.init();
   });
 });
--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,15 @@
-require('@babel/polyfill')
+require('@babel/polyfill');
 
-const HDWalletProvider = require('@truffle/hdwallet-provider')
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
-function getProvider (keysFile, rpcUrl) {
-  let keys = {}
+function getProvider(keysFile, rpcUrl) {
+  let keys = {};
   try {
-    keys = require(keysFile)
-    return new HDWalletProvider(keys.privKey, rpcUrl)
+    keys = require(keysFile);
+    return new HDWalletProvider(keys.privKey, rpcUrl);
   } catch (err) {
-    console.log(err)
-    console.log('could not find ' + keysFile)
+    console.log(err);
+    console.log('could not find ' + keysFile);
   }
 }
 
@@ -28,41 +28,50 @@ module.exports = {
   },
 
   networks: {
-   
-  
     local: {
       host: 'localhost',
       port: 8545,
-      network_id: '85458545',
+      network_id: '85458545'
     },
-
 
     ganache: {
       host: 'localhost',
       port: 7545,
-      network_id: '5777',
+      network_id: '5777'
     },
 
     kovan_infura: {
-      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://kovan.infura.io/v3/46919ce6487b4a8b83d0a870ad59588c"),
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNENOMIC,
+          'https://kovan.infura.io/v3/46919ce6487b4a8b83d0a870ad59588c'
+        ),
       network_id: 42,
-      gas:          6000000,
+      gas: 6000000,
       gasPrice: 10000000000
-    },   
+    },
     rinkeby_infura: {
-      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://rinkeby.infura.io/v3/46919ce6487b4a8b83d0a870ad59588c"),
-      network_id: 4,
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNENOMIC,
+          'https://rinkeby.infura.io/v3/46919ce6487b4a8b83d0a870ad59588c'
+        ),
+      network_id: 4
     },
 
     mainnet_infura: {
-      provider: () => new HDWalletProvider(process.env.MNENOMIC, "https://mainnet.infura.io/v3/46919ce6487b4a8b83d0a870ad59588c"),
+      provider: () =>
+        new HDWalletProvider(
+          process.env.MNENOMIC,
+          'https://mainnet.infura.io/v3/46919ce6487b4a8b83d0a870ad59588c'
+        ),
       network_id: 1,
       gas: 0x2fefd5
     }
   },
 
   mocha: {
-    useColors: true,
+    useColors: true
     // Crashing since version update - no solution yet
     // reporter: 'eth-gas-reporter',
     // reporterOptions : {
@@ -70,4 +79,4 @@ module.exports = {
     //   gasPrice: 21
     // }
   }
-}
+};
